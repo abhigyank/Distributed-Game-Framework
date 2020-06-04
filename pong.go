1b01566eefd8145ae4b80addfe5372ff5a318900
Abhigyan Khaund <20434085+abhigyank@users.noreply.github.com>
2020-06-04 19:43:42 +0530

    Add single player pong game.
    Source: https://github.com/karreiro/pong-go

---
+++ pong.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+
+	"./types"
+)
+
+const winWidth, winHeight = 800, 600
+
+func main() {
+
+	initEverything()
+	defer sdl.Quit()
+
+	window := createWindow("Pong - Game", winWidth, winHeight)
+	defer window.Destroy()
+
+	renderer := createRenderer(window)
+	defer renderer.Destroy()
+
+	texture := createTexture(renderer)
+	defer texture.Destroy()
+
+	white := types.Color{R: 255, G: 255, B: 255}
+	pixels := make([]byte, winWidth*winHeight*4)
+
+	player1 := types.Paddle{Position: types.Position{X: 50, Y: 100}, Width: 20, Height: 100, Color: white}
+	player2 := types.Paddle{Position: types.Position{X: 750, Y: 100}, Width: 20, Height: 100, Color: white}
+	ball := types.Ball{Position: types.Position{X: 400, Y: 300}, Radius: 20, XVelocity: 10, YVelocity: 10, Color: white}
+	running := true
+	keyState := sdl.GetKeyboardState()
+
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+				break
+			}
+		}
+
+		clear(pixels)
+
+		player1.Update(keyState)
+		player2.AiUpdate(&ball)
+		ball.Update(&player1, &player2)
+
+		player1.Draw(pixels)
+		player2.Draw(pixels)
+		ball.Draw(pixels)
+
+		texture.Update(nil, pixels, winWidth*4)
+		renderer.Copy(texture, nil, nil)
+		renderer.Present()
+
+		sdl.Delay(24)
+	}
+}
+
+func clear(pixels []byte) {
+	for i := range pixels {
+		pixels[i] = 0
+	}
+}
+
+func createTexture(renderer *sdl.Renderer) *sdl.Texture {
+	texture, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, winWidth, winHeight)
+	if err != nil {
+		panic(err)
+	}
+	return texture
+}
+
+func createRenderer(window *sdl.Window) *sdl.Renderer {
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		panic(err)
+	}
+	return renderer
+}
+
+func createWindow(title string, width, height int32) *sdl.Window {
+	window, err := sdl.CreateWindow(title, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, width, height, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	return window
+}
+
+func initEverything() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+}
---
+++ types/types.go
@@ -0,0 +1,113 @@
+package types
+
+import (
+	"os"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const winWidth, winHeight = 800, 600
+
+// Color represents the color of a given pixel.
+type Color struct {
+	R, G, B byte
+}
+
+// Position represents the position of a given element.
+type Position struct {
+	X, Y float32
+}
+
+// Ball represents the ball of the game.
+type Ball struct {
+	Position
+	Radius    int
+	XVelocity float32
+	YVelocity float32
+	Color     Color
+}
+
+// Paddle represents a player in the game.
+type Paddle struct {
+	Position
+	Width  int
+	Height int
+	Color  Color
+}
+
+// Draw draws the ball.
+func (ball *Ball) Draw(pixels []byte) {
+	for y := -ball.Radius; y < ball.Radius; y++ {
+		for x := -ball.Radius; x < ball.Radius; x++ {
+			if x*x+y*y < ball.Radius*ball.Radius {
+				setPixel(int(ball.X)+x, int(ball.Y)+y, ball.Color, pixels)
+			}
+		}
+	}
+}
+
+// Update updates the ball position and controls collision.
+func (ball *Ball) Update(leftPaddle *Paddle, rightPaddle *Paddle) {
+
+	ball.Y += ball.YVelocity
+	ball.X += ball.XVelocity
+
+	if int(ball.Y) < 0+ball.Radius || int(ball.Y) > winHeight-ball.Radius {
+		ball.YVelocity = -ball.YVelocity
+	}
+
+	if int(ball.X) < 0+ball.Radius || int(ball.X) > winWidth-ball.Radius {
+		println("You lose!")
+		os.Exit(0)
+	}
+
+	if ball.X < leftPaddle.X+float32(leftPaddle.Width/2)+float32(ball.Radius) {
+		if ball.Y > leftPaddle.Y-float32(leftPaddle.Height/2)-float32(ball.Radius) && ball.Y < leftPaddle.Y+float32(leftPaddle.Height/2)+float32(ball.Radius) {
+			ball.XVelocity = -ball.XVelocity
+		}
+	}
+
+	if ball.X > rightPaddle.X-float32(rightPaddle.Width/2)-float32(ball.Radius) {
+		if ball.Y > rightPaddle.Y-float32(rightPaddle.Height/2)-float32(ball.Radius) && ball.Y < rightPaddle.Y+float32(rightPaddle.Height/2)+float32(ball.Radius) {
+			ball.XVelocity = -ball.XVelocity
+		}
+	}
+}
+
+// Draw draws the paddle.
+func (paddle *Paddle) Draw(pixels []byte) {
+	startX := int(paddle.X) - paddle.Width/2
+	startY := int(paddle.Y) - paddle.Height/2
+
+	for y := 0; y < paddle.Height; y++ {
+		for x := 0; x < paddle.Width; x++ {
+			setPixel(startX+x, startY+y, paddle.Color, pixels)
+		}
+	}
+}
+
+// Update updates the paddle position by checking the keyboard state.
+func (paddle *Paddle) Update(keyState []uint8) {
+	if keyState[sdl.SCANCODE_UP] != 0 {
+		paddle.Y -= 10
+	}
+
+	if keyState[sdl.SCANCODE_DOWN] != 0 {
+		paddle.Y += 10
+	}
+}
+
+// AiUpdate updates the paddle position by using a simple AI.
+func (paddle *Paddle) AiUpdate(ball *Ball) {
+	paddle.Y = ball.Y
+}
+
+func setPixel(x, y int, c Color, pixels []byte) {
+	index := (y*winWidth + x) * 4
+
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.R
+		pixels[index+1] = c.G
+		pixels[index+2] = c.B
+	}
+}
